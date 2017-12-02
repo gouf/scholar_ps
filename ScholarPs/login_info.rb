112ffd2eb2a6189813ce8b73f4ab279ff8dbad0e
gouf <innocent.zero@gmail.com>
2017-12-02 14:00:51 +0900

    Deactivate value check

--- ScholarPs/login_info.rb
+++ ScholarPs/login_info.rb
@@ -7,16 +7,5 @@ module ScholarPs
     UserId = ENV['USER_ID'].dup.freeze
     Password = ENV['PASSWORD'].dup.freeze
     LoanId = ENV['LOAN_ID'].dup.freeze
-
-    # FIXME: Move to other Class or method difinition
-    # The scraping data exists after authentication
-    lambda do
-      const_list =
-        constants.map do |name|
-          [name, const_get(name).to_s.length.zero?]
-        end
-      empty_const = const_list.find_all(&:last)
-      raise "All of The LoginInfo constant values should not be empty.(#{empty_const.map(&:first).join(',')})" unless empty_const.size.zero?
-    end.()
   end
 end
